c70ea02e6799fafc6e04413776bef9dbac4347cb
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2022-09-22 21:37:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 #tmux new-window -t  hector:1 'python3 src/NeoPixel.py'
 #python3 src/main.py
 
-cd /home/pi/Satoshi24/src
+cd /home/pi/ChicksOnTheBlocks/src
 KIVY_GL_BACKEND=gl /usr/bin/python3 main.py &> last-run.log 2>&1
